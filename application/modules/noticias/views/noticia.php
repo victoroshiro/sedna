261b98854191997cd5085289de7730fed6f4cb57
eduruiz <contato@eduruiz.com.br>
2017-09-04 11:28:28 -0300

    alterar facebook para correto

--- application/modules/noticias/views/noticia.php
+++ application/modules/noticias/views/noticia.php
@@ -60,7 +60,7 @@
 							  fjs.parentNode.insertBefore(js, fjs);
 							}(document, 'script', 'facebook-jssdk'));</script>
 
-							<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+							<div class="fb-page" data-href="https://www.facebook.com/cimitarrayachts/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cimitarrayachts/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cimitarrayachts/">Facebook</a></blockquote></div>
 						</div>
 					</aside>
 					<!-- //OneFourth -->
