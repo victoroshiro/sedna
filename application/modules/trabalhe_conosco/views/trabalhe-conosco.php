625c785882b4cef0dbbf4e4233eefc96a7d1c0bc
eduruiz <contato@eduruiz.com.br>
2018-02-07 11:31:10 -0200

    fechar a tag do linkedin no link trabalh-conosco

--- application/modules/trabalhe_conosco/views/trabalhe-conosco.php
+++ application/modules/trabalhe_conosco/views/trabalhe-conosco.php
@@ -1,63 +1,68 @@
 <main class="main" role="main">
 <!-- Content -->
-	<!-- Intro -->
-	<header class="intro">
-		<div class="wrap">
-			<h1>Trabalhe conosco</h1>
-		</div>
-	</header>
-	<!-- //Intro -->
-	<div class="content boxed grid2">
-		<!-- Item -->
-		<article class="full-width hentry">
-			<div class="one-half">
-				<div class="text">
-					<p>Acesse o nosso Linkedln: <a href="https://www.linkedin.com/company/cimitarrayachts/" target="_blank"><i class="fa fa-linkedin-square trabalhe"></i></p>
-					<form method="post" action="<?php echo site_url('trabalhe-conosco/send') ?>" name="contactform" id="contactform" class="workhere">
-						<fieldset>
-							<div id="message_result"></div>
-							<div class="full-width">
-								<label for="name">Nome e Sobrenome</label>
-								<input type="text" name="name" id="name"/>
-							</div>
-							
-							<div class="full-width">
-								<label for="email">E-mail</label>
-								<input type="email" name="email" id="email"/>
-							</div>
-							
-							<div class="full-width">
-								<label for="phone">Telefone</label>
-								<input type="text" name="phone" id="phone"/>
-							</div>
-                                                        
-                            <div class="full-width">
-								<label for="area_interesse">Área de Interesse</label>
-								<input type="text" name="area_interesse" id="area_interesse"/>
-							</div>
-                                                        
-                            <div class="full-width">
-								<label for="message">Sua Mensagem</label>
-								<textarea id="message" name="message"></textarea>
-							</div>
-							
-                            <div class="full-width">
-								<label for="comments">Anexe seu Currículo</label>
-                                <input type="file" name="curriculo">
-							</div>
+<!-- Intro -->
+<header class="intro">
+    <div class="wrap">
+        <h1>Trabalhe conosco</h1>
+    </div>
+</header>
+<!-- //Intro -->
+<div class="content boxed grid2">
+    <!-- Item -->
+    <article class="full-width hentry">
+        <div class="one-half">
+            <div class="text">
+                <p>
+                    Acesse o nosso Linkedln:
+                    <a href="https://www.linkedin.com/company/cimitarrayachts/" target="_blank">
+                        <i class="fa fa-linkedin-square trabalhe"></i>
+                    </a>
+                </p>
+                <form method="post" action="<?php echo site_url('trabalhe-conosco/send') ?>" name="contactform" id="contactform" class="workhere">
+                    <fieldset>
+                        <div id="message_result"></div>
+                        <div class="full-width">
+                            <label for="name">Nome e Sobrenome</label>
+                            <input type="text" name="name" id="name"/>
+                        </div>
 
-							<div class="full-width">
-								<input type="submit" value="Enviar Mensagem" class="button gold large" id="submit" />
-							</div>
-						</fieldset>
-					</form>
-				</div>
-			</div>
-			<div class="one-half">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.1637887839734!2d-51.199447385222065!3d-30.032158581886776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197834f02e75ed%3A0x39ab3c65d4e274f7!2sR.+Quintino+Bocai%C3%BAva%2C+1587+-+Floresta%2C+Porto+Alegre+-+RS%2C+90440-051!5e0!3m2!1sen!2sbr!4v1513014499017" width="1000" height="800" frameborder="0" style="border:0"></iframe>
-			</div>
-		</article>
-		<!-- //Item -->
-	</div>
-	<!-- //Content-->
+                        <div class="full-width">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" id="email"/>
+                        </div>
+
+                        <div class="full-width">
+                            <label for="phone">Telefone</label>
+                            <input type="text" name="phone" id="phone"/>
+                        </div>
+
+                        <div class="full-width">
+                            <label for="area_interesse">Área de Interesse</label>
+                            <input type="text" name="area_interesse" id="area_interesse"/>
+                        </div>
+
+                        <div class="full-width">
+                            <label for="message">Sua Mensagem</label>
+                            <textarea id="message" name="message"></textarea>
+                        </div>
+
+                        <div class="full-width">
+                            <label for="comments">Anexe seu Currículo</label>
+                            <input type="file" name="curriculo">
+                        </div>
+
+                        <div class="full-width">
+                            <input type="submit" value="Enviar Mensagem" class="button gold large" id="submit" />
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        <div class="one-half">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.1637887839734!2d-51.199447385222065!3d-30.032158581886776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197834f02e75ed%3A0x39ab3c65d4e274f7!2sR.+Quintino+Bocai%C3%BAva%2C+1587+-+Floresta%2C+Porto+Alegre+-+RS%2C+90440-051!5e0!3m2!1sen!2sbr!4v1513014499017" width="1000" height="800" frameborder="0" style="border:0"></iframe>
+        </div>
+    </article>
+    <!-- //Item -->
+</div>
+<!-- //Content-->
 </main>
