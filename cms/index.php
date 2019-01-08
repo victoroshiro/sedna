adb1b5b8bc9cfd2c7a3c5885da4a91154e0ab5c5
mdd300 <victoroshiro_mdd300@hotmail.com>
2019-01-08 15:58:09 -0200

    ajustes

--- cms/index.php
+++ cms/index.php
@@ -225,7 +225,7 @@ switch (ENVIRONMENT)
 	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 	// Path to the system folder
-	define('BASEPATH', str_replace('\\', '/dev', $system_path));
+	define('BASEPATH', str_replace('\\', '/', $system_path));
 
 	// Path to the front controller (this file)
 	define('FCPATH', dirname(__FILE__).'/');
