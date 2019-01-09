2433cd0591e74601893a23480d45af0d8677ac15
mdd300 <victoroshiro_mdd300@hotmail.com>
2019-01-09 16:33:53 -0200

    ajustes

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -84,7 +84,7 @@
                     <section class="cta ">
                             <div class="wrap center">
                                     <div class="text-center">
-                                            <h2>C O L E Ç Ã O  S E D N A  G R O U P</h2>
+                                            <h2>C O L E Ç Ã O    S E D N A    G R O U P</h2>
                                     </div>
                             </div>
                             <div class="wrap wrap-flex">
